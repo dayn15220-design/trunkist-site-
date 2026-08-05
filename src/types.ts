b14b0e7846d9f5e49a8989deff0711ba753e5d74
dayn15220-design <dayn15220@gmail.com>
2026-08-06 08:17:02 +0900

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -5,12 +5,3 @@ export type TabType =
   | 'executor-console';
 
 export type LanguageType = 'ru' | 'en';
-
-export interface TrunkistFeature {
-  nameRu: string;
-  nameEn: string;
-  descRu: string;
-  descEn: string;
-  tab: 'Main' | 'Aim' | 'Physics' | 'Visuals' | 'Sound' | 'Settings';
-  defaultState: boolean | string | number;
-}
