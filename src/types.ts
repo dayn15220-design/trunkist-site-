778a31ad1efa9df1c86afab8889e65f8c022e33a
dayn15220-design <dayn15220@gmail.com>
2026-08-07 09:28:37 +0900

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,19 @@
-export type TabType = 'features-overview' | 'get-script' | 'parry-arena' | 'weapon-dps' | 'executor-console';
+export type TabType =
+  | 'features-overview'
+  | 'live-telemetry'
+  | 'live-chat'
+  | 'ui-customizer'
+  | 'parry-arena'
+  | 'weapon-dps'
+  | 'executor-console';
+
 export type LanguageType = 'ru' | 'en';
+
+export interface TrunkistFeature {
+  nameRu: string;
+  nameEn: string;
+  descRu: string;
+  descEn: string;
+  tab: 'Main' | 'Aim' | 'Physics' | 'Visuals' | 'Sound' | 'Settings';
+  defaultState: boolean | string | number;
+}
